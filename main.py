e83a27382f65637231c6d0b4b055f1c01872b689
asseternity <nakupovasset@gmail.com>
2025-09-27 17:26:49 +0400

    feat: update project structure, update to do list

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ import seaborn as sns
 import re
 from thefuzz import process
 
-
 def normalize_country(name: str) -> str:
     if pd.isna(name):
         return None
@@ -658,25 +657,6 @@ plt.xlabel("Strength of Correlation with Happiness (absolute)")
 plt.title("Which metrics affect Happiness most (color shows + / -)")
 plt.subplots_adjust(left=0.35)
 plt.show()
-
-# [v] fix nulls in the data --- see dummy db
-# [v] add to dictionary: ivory coast, south korea, guinea-bissau, russia, belarus, eswatini, czechia
-# [v] test other countries with no data on the website and add them to the leniency dictionary
-# [v] calculate "weighted expected levels for each country's level of happiness" for each aspect to later calculate outlier numbers
-# [v] export effect coefficients as well
-# [_] finish adding the previous datasets 
-# [_] add more tables / metrics for more opportunities to find correlations:
-# Cost of living: ???
-# GDP per capita: https://www.kaggle.com/datasets/nitishabharathi/gdp-per-capita-all-countries
-# Military spending: https://www.kaggle.com/datasets/nitinsss/military-expenditure-of-countries-19602019
-# Homicide rate: https://www.kaggle.com/datasets/bilalwaseer/countries-by-intentional-homicide-rate
-# Geography variables: https://www.kaggle.com/datasets/zanderventer/environmental-variables-for-world-countries?select=World_countries_env_vars.csv
-# Average age: https://www.kaggle.com/datasets/divyansh22/average-age-of-countries
-# Inflation: https://www.kaggle.com/datasets/meeratif/inflation-2022
-
-# [_] post on kaggle
-# [_] add to portfolio
-# [_] post about it on linked in
 
 # exporting to json
 def export_as_json():
---
+++ to_do.py
@@ -0,0 +1,17 @@
+# [v] fix nulls in the data --- see dummy db
+# [v] add to dictionary: ivory coast, south korea, guinea-bissau, russia, belarus, eswatini, czechia
+# [v] test other countries with no data on the website and add them to the leniency dictionary
+# [v] calculate "weighted expected levels for each country's level of happiness" for each aspect to later calculate outlier numbers
+# [v] export effect coefficients as well
+# [_] finish adding the previous datasets 
+# [_] add more tables / metrics for more opportunities to find correlations:
+# Cost of living: ???
+# GDP per capita: https://www.kaggle.com/datasets/nitishabharathi/gdp-per-capita-all-countries
+# Military spending: https://www.kaggle.com/datasets/nitinsss/military-expenditure-of-countries-19602019
+# Homicide rate: https://www.kaggle.com/datasets/bilalwaseer/countries-by-intentional-homicide-rate
+# Geography variables: https://www.kaggle.com/datasets/zanderventer/environmental-variables-for-world-countries?select=World_countries_env_vars.csv
+# Average age: https://www.kaggle.com/datasets/divyansh22/average-age-of-countries
+# Inflation: https://www.kaggle.com/datasets/meeratif/inflation-2022
+# [_] add UNITS to column names
+# [_] compile a table of sources: ask LLM
+# [_] post on kaggle
